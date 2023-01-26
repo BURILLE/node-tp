b3eaaecae4138f2cc5add4caf65252c7146c5ee2
BURILLE <cecile.burille@etu.umontpellier.fr>
2023-01-26 16:41:51 +0100

    Fin du TP3

--- CasPratique/index.js
+++ CasPratique/index.js
@@ -1,8 +1,32 @@
 const express = require('express')
+const util = require("util");
+const fs = require("fs");
 const app = express()
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
+const axios = require("axios")
+
+
+const nunjucks = require('nunjucks')
+
+
+app.get('/myspecialusers',async function(req, res) {
+
+    const user1 = axios.get('https://jsonplaceholder.typicode.com/users/1')
+    const user2 = axios.get('https://jsonplaceholder.typicode.com/users/4')
+    const user3 = axios.get('https://jsonplaceholder.typicode.com/users/6')
+
+    const [response1, response2, response3] = await Promise.all([user1, user2, user3])
+
+    const reponse = await nunjucks.render('index.html', {
+        user1: response1.data,
+        user2: response2.data,
+        user3: response3.data
+
+    })
+
+    res.send(reponse)
+
+
 })
 
 
